84c9af643aae556da86ffbdd5aa166771e224b2a
0ec2b5 <0ec2b5@users.noreply.github.com>
2025-04-26 01:50:05 +0000

    nix: update flake

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
               src = (builtins.fetchTarball {
                 url =
                   "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-                sha256 = "1nbdyif2j6jbbd4c0nczr89mvd2d60w8q3wj0wv9ycksikhswvy5";
+                sha256 = "1fdjpm6i66n74cd2bfb2brmilj1842dig2fjxg4r9ximlkl0kf50";
               });
               version = "latest";
 
